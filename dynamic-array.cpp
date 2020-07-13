66ecce5384aecf980834ecb1d7b381c6ff02a450
Pranav Patil <4pranavpatil@gmail.com>
2020-07-12 20:21:55 -0700

    Dynamic array allocates memory on heapw

--- dynamic-array.cpp
+++ dynamic-array.cpp
@@ -1,10 +1,10 @@
 #include <stdexcept>
+#include <iostream>
 
 /**
 A dynamic array will double in size every time the size reaches capacity.
 An amortized analysis shows that adding an element is still O(1).
 */
-
 
 const int START_SIZE = 20;
 
@@ -13,43 +13,48 @@ class DynamicArray {
   private:
     int size = 0;
     int capacity = START_SIZE;
-    T values[START_SIZE];
+    // pointer to beginning of array
+    T *values;
   public:
+    DynamicArray() {
+      values = new T[capacity];
+    }
     void resize(int n) {
       // if shrinking, then only copy first n
       int copyAmt = size < n ? size : n;
       capacity = n;
-      T bigger[capacity];
+      T *bigger = new T[capacity];
       for (int i = 0; i < size; i ++) {
-        bigger[i] = values[i];
+        *(bigger+i) = *(values+i);
       }
+      delete [] values;
       values = bigger;
     }
     T &get(int i) {
       if (i < size) {
-        return values[i];
+        return *(values+i);
       } else {
         throw new std::out_of_range("Array index out of bounds");
       }
     };
     T &operator[] (int i) {
-      this.get(i);
+      return get(i);
     }
     void set(int i, T val) {
       if (i < size) {
-        values[i] = val; 
+        *(values+i) = val; 
       } else {
         throw new std::out_of_range("Array index out of bounds");
       }
     };
     void push_back(T val) {
-      if (size == capacity) {
+      if (size == capacity - 1) {
         capacity *= 2;
         resize(capacity);
-        values[size] = val;
+        *(values+size) = val;
         size ++;
       } else {
-        values[size] = val;
+        *(values+size) = val;
         size ++;
       }
     }
@@ -58,4 +63,9 @@ class DynamicArray {
 // example usage
 int main () {
   DynamicArray<int> z;
+  z.resize(5);
+  z.push_back(0);
+  z.push_back(1);
+  z[1] = 2;
+  std::cout << z[1] << "\n";
 }
